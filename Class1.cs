71d23d812db68da0546fbd6f4bd45fa5a45865db
pjs70776 <patrick.j.stover.1985@gmail.com>
2024-08-13 10:33:47 -0700

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodsAssignment
+{
+    public class Class1
+    {
+
+
+        //create three methods, each of which will take one integer parameter
+        //in and return an integer. The methods should do some math operation
+        //on the received parameter.
+        //three methods inside Class1
+        //method1
+        public static int method1(int x) 
+        {
+            int Add = x + 5; return Add;
+        }
+        //method 2
+        public static int method2(int y) 
+        {
+            int Multiply = y * 5; return Multiply;
+        }
+        //method 3
+        public static int method3(int z)
+        {
+            int Divide = z / 5; return Divide;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodsAssignment
+{
+    class Program
+    {
+        static void Main()
+        {
+            //ask the user what number they want to do the math
+            //operations on.
+            //method 1
+            Console.WriteLine("What number do you want to select to do the math operation?");
+            Console.WriteLine("Enter a number to be added to 5");
+            int additon = Convert.ToInt32(Console.ReadLine());
+            int sum = Class1.method1(additon);
+            Console.WriteLine(sum + " is the answer.");
+
+            //method 2
+            Console.WriteLine("Enter a number to be multiplied by 5");
+            int multi = Convert.ToInt32(Console.ReadLine());
+            int product = Class1.method2(multi);
+            Console.WriteLine(product + " is the answer.");
+
+            //method 3
+            Console.WriteLine("Enter a number to be divided by 5");
+            int div = Convert.ToInt32(Console.ReadLine());
+            int quot = Class1.method3(div);
+            Console.WriteLine(quot + " is the answer.");
+            Console.ReadLine();
+        }
+       
+    }    
+}
